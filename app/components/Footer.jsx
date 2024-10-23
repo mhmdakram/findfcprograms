814ad13ea4dca725de09de146b2a31edbb516111
RahmathulSana <sanarahmathul@gmail.com>
2024-10-23 17:33:22 +0530

    footer added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
 
-function Footer() {
+import React from 'react';
+
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-custom-bg text-black py-4">
+      <div className="max-w-screen-xl  mx-auto px-4">
+        <img src="/assets/logo.png" className="h-24 w-auto flex justify-center align-middle ml-4 mt-4"></img>
+       
+        <ul className="flex align-middle  justify-end space-x-4  ">
+          <li>
+            <a href="/about" className="text-black hover:text-green-300">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className="text-black hover:text-green-300">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="/faq" className="text-black hover:text-green-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="/faq" className="text-black hover:text-green-300">
+              Faq
+            </a>
+          </li>
+        </ul>
+        <p className="text-center text-xs ">
+         Copyright &copy;  FindFC. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'custom-bg': '#cdcdcd',
+       
       },
     },
   },
